9818dbf75725837e778ee616fb8aa325b4cfd589
kb-kabelo <kabelolekalakala063@gmail.com>
2025-09-01 22:27:49 +0200

    Initial commit with Python files

---
+++ main.py
@@ -0,0 +1,45 @@
+running = True
+board_size = 15
+wall = "#"
+
+board = [
+    [" " for _ in range(board_size)] for _ in range(board_size)
+]  # 15*15 board of " "
+
+board[2][2] = "@"  # Random point for vibes
+
+
+def board_display():
+    for i in range(15):
+        board[0][i] = wall  # top row
+        board[14][i] = wall  # bottom row
+        board[i][0] = wall  # left column
+        board[i][-1] = wall  # right column
+
+    for row in board:
+        print(" ".join(row))
+
+
+if __name__ == "__main__":
+    while running:
+        board_display()
+
+# Outcome
+"""
+####################
+#                  #
+#     oooo>        #
+#                  #
+#          *       #
+#                  #
+####################
+"""
+# Additions
+"""
+improve the display function
+find a way to clear terminal
+move something
+food 
+classes
+done
+"""
